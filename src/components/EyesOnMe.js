910ce8ba124376863db3d999e3e5812dc88982c8
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-14 18:29:38 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    const blur = ()=>{
+        console.log("Hey! Eyes on me!")
+    }
+    const focus = ()=>{
+        console.log("Good!")
+    }
+  return (
+    <div>
+        <button onBlur={blur} onFocus={focus}>Eyes on me</button>
+
+        
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const Keypad = () => {
+    const onchange = ()=>{
+        console.log("Entering password...")
+    }
+  return (
+    <div>
+        <input  type="password" onChange={onchange}/>
+
+    </div>
+  )
+}
+
+export default Keypad
